943fd4c4accdba8343440e999a70b5a539c50929
Irvandzawinnuha <72366333+Irvandzawinnuha@users.noreply.github.com>
2024-03-24 19:14:11 +0700

    menambahkan class SayaTubeVideo

---
+++ tp6/tp6/Program.cs
@@ -0,0 +1,10 @@
+﻿using tp6;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SayaTubeVideo vid = new SayaTubeVideo("Tutorial Design By Contract - Irvan Dzawin Nuha");
+        vid.printVideoDetails();
+    }
+}
---
+++ tp6/tp6/SayaTubeVideo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tp6
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        private string title;
+        private int playCount;
+
+        public SayaTubeVideo(string title)
+        {
+            this.title = title;
+            Random randomId = new Random();
+            id = randomId.Next(10000, 99999);
+        }
+
+        public void increasePlayCount(int increment)
+        {
+            try
+            {
+                checked
+                {
+                    playCount += increment;
+                    if (playCount + increment > 10000000) throw new ArgumentOutOfRangeException();
+                }
+            }
+
+            catch (ArgumentOutOfRangeException argex)
+            {
+                Console.WriteLine(argex.Message);
+            }
+
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public void printVideoDetails()
+        {
+            Console.WriteLine("ID: " + id + "\n" + "Judul: " + title + "\n" + "Play count: " + playCount + "\n");
+        }
+
+
+    }
+}
